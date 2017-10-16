23bad08fc94129dad33872adb9cc81e3c33da975
Vladyslav Kalischuk <elektronikerv@gmail.com>
2017-10-17 00:07:48 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,60 +1,40 @@
 package sample;
 
-
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    final static int PADDING = 1;
-
-
+    private SimpleIntegerProperty scoreProperty = new SimpleIntegerProperty(0);
     @Override
-    public void start(Stage stage) throws InterruptedException {
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(PADDING));
-        grid.setHgap(PADDING);
-        grid.setVgap(PADDING);
+    public void start(Stage stage) {
 
-        grid = Controller.initMap();
-        Label title = new Label("Score: ");
-        Label l = new Label();
-        title.setLabelFor(l);
-        SimpleIntegerProperty i = new SimpleIntegerProperty(0);
-        l.textProperty().bind(i.asString());
-        grid.add(title, 9, 0);
-        grid.add(l, 10, 0);
-
-        Scene scene = new Scene(grid, 600, 600);
-        stage.setScene(scene);
-        stage.setTitle("Color Lines");
-        stage.setResizable(false);
-        stage.show();
+        Game game = new Game();
+        initStage(game, stage, game.getSize());
+        stage.setOnCloseRequest(event -> Platform.exit());
 
 
-        Thread checkThread = new Thread(()-> {
+        Thread checkingThread = new Thread(()-> {
             final boolean[] b = {false};
             while(!b[0]) {
-                Controller.checkHorizontal();
-                Controller.checkVertical();
+                game.checkHorizontal();
+                game.checkVertical();
 
                 Platform.runLater(() -> {
-                    i.set(Controller.getScore());
-                    b[0] = Controller.isLosed();
-                    if(b[0]) {
-                        Alert a = new Alert(Alert.AlertType.CONFIRMATION);
-                        a.show();
-                    }
+                    scoreProperty.set(game.getScore());
+                    b[0] = game.isLosed();
+                    if (b[0])
+                        showLoseMessage();
                 });
 
                 try {
@@ -65,15 +45,42 @@ public class Main extends Application {
                 }
             }
         });
-        stage.setOnCloseRequest(event ->
-                        System.exit(0)
-        );
-
-        checkThread.start();
-
-
+        checkingThread.start();
     }
 
+    private void initStage(Game game, Stage stage, int size) {
+        GridPane grid = game.initMap();
+
+        Label title = new Label("Score: ");
+        Label scoreLabel = new Label("0000");
+
+        Font font = new Font("Times New Roman", 20);
+        title.setFont(font);
+        scoreLabel.setFont(font);
+        title.setLabelFor(scoreLabel);
+        scoreLabel.textProperty().bind(scoreProperty.asString());
+
+        HBox hBox = new HBox();
+        hBox.getChildren().addAll(title, scoreLabel);
+        hBox.setAlignment(Pos.CENTER);
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(hBox, grid);
+
+        Scene scene = new Scene(vBox);
+        stage.setScene(scene);
+        stage.setTitle("Color Lines");
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    private void showLoseMessage() {
+        Alert loseMessage = new Alert(Alert.AlertType.INFORMATION);
+        loseMessage.setTitle("You lose!");
+        loseMessage.setHeaderText("You lose! Your score: " + scoreProperty.get());
+        loseMessage.showAndWait();
+        Platform.exit();
+    }
 
     public static void main(String[] args) {
         launch(args);
